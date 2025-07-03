abcfd7c76316880c60a6aa8c8d695db88a186108
Joseph Olaitan Felix <destechinnovation@gmail.com>
2025-07-03 03:13:26 +0000

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,22 +1,50 @@
 -- File: database_index.sql
 
--- Index on users.id to speed up JOINs and lookups by user
+-- BEFORE indexing: Measure performance of user-booking join
+EXPLAIN ANALYZE
+SELECT
+    u.username,
+    COUNT(b.booking_id) AS total_bookings
+FROM
+    users AS u
+JOIN
+    bookings AS b ON u.id = b.user_id
+GROUP BY
+    u.username
+ORDER BY
+    u.username;
+
+-- Create index on users.id (used in JOINs)
 CREATE INDEX idx_users_id ON users(id);
 
--- Index on users.username to optimize ORDER BY and lookups
+-- Create index on users.username (used in ORDER BY)
 CREATE INDEX idx_users_username ON users(username);
 
--- Index on bookings.user_id to speed up JOINs and filters by user
+-- Create index on bookings.user_id (used in JOINs)
 CREATE INDEX idx_bookings_user_id ON bookings(user_id);
 
--- Index on bookings.property_id to optimize JOINs with properties
+-- Create index on bookings.property_id (used in JOINs)
 CREATE INDEX idx_bookings_property_id ON bookings(property_id);
 
--- Index on bookings.start_date and end_date for filtering and sorting
+-- Create compound index for filtering dates
 CREATE INDEX idx_bookings_start_end_date ON bookings(start_date, end_date);
 
--- Index on properties.id to improve JOIN performance
+-- Create index on properties.id (used in JOINs)
 CREATE INDEX idx_properties_id ON properties(id);
 
--- Index on properties.property_name to help ORDER BY and text search
+-- Create index on properties.property_name (used in ORDER BY)
 CREATE INDEX idx_properties_name ON properties(property_name);
+
+-- AFTER indexing: Re-run same query to check performance improvement
+EXPLAIN ANALYZE
+SELECT
+    u.username,
+    COUNT(b.booking_id) AS total_bookings
+FROM
+    users AS u
+JOIN
+    bookings AS b ON u.id = b.user_id
+GROUP BY
+    u.username
+ORDER BY
+    u.username;
